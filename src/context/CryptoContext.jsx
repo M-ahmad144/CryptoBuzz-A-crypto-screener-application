38feb76f80b8646f781434ca4aaee0de2714da38
Ahmad <muhammadahmadmughal0987@gmail.com>
2024-07-04 20:53:15 +0500

    Context API In Integration

---
+++ src/context/CryptoContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useLayoutEffect, useState } from "react";
+
+// Create context object
+export const CryptoContext = createContext({});
+
+// Create the provider component
+export const CryptoProvider = ({ children }) => {
+  const [cryptoData, setCryptoData] = useState();
+
+  const getCryptoData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+      );
+      const data = await response.json();
+      console.log(data);
+      setCryptoData(data);
+    } catch (error) {
+      console.error("Error fetching crypto data:", error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    getCryptoData();
+  }, []);
+  return (
+    // Provider component
+    <CryptoContext.Provider value={{ cryptoData }}>
+      {children}
+    </CryptoContext.Provider>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import {Outlet} from 'react-router-dom';
-import Logo from '../components/Logo';
-import Navigation from '../components/Navigation';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+import { CryptoProvider } from "../context/CryptoContext";
 
-export default function Home () {
+export default function Home() {
   return (
-    <main className="h-full  w-full flex flex-col first-letter: content-center items-center 
-           relative text-white font-nunito">
-      <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
-      <Logo />
-      <Navigation />
-      <Outlet />
-    </main>
+    // the Home Component is my Main root  component so the CryptoProvider warps it out
+    <CryptoProvider>
+      <main
+        className="h-full  w-full flex flex-col first-letter: content-center items-center 
+           relative text-white font-nunito"
+      >
+        <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+        <Logo />
+        <Navigation />
+        <Outlet />
+      </main>
+    </CryptoProvider>
   );
 }
